d2b9ac2d86976b5b129fbf05c0ac497d8dd59ad3
Mujahid Ansori Majid <hitzveraidn@gmail.com>
2023-11-17 17:32:40 +0700

    add login functionality mvp

--- src/components/atoms/ButtonLogin/index.jsx
+++ src/components/atoms/ButtonLogin/index.jsx
@@ -1,10 +1,8 @@
-export default function ButtonLogin({ type, src, tittle, className }){
-    return(
-        <button className={className} type={type}>
-        <img className="login_fb" src={src} alt=""/>
-        <span className="login_button_tittle_fb">
-            {tittle}
-        </span>
+export default function ButtonLogin({ type, src, tittle, className }) {
+  return (
+    <button className={className} type={type}>
+      <img className="login_fb" src={src} alt="" />
+      <span className="login_button_tittle_fb">{tittle}</span>
     </button>
-    )
+  );
 }
